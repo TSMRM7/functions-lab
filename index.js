0f8ef8834005d3308295a0fd8651211f170b45d0
ryanruri <muiruri13ryan@gmail.com>
2024-09-24 13:02:43 +0300

    initial git

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+
+const headquarters = 42;
+function distanceFromHqInBlocks(street) {
+  return Math.abs(street - headquarters);
+}
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distanceInFeet = distanceTravelledInFeet(start, destination);
+  
+  if (distanceInFeet <= 400) {
+    return 0; 
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return (distanceInFeet - 400) * 0.02;
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far'; 
+  }
+}
